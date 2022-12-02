51bac54a8247b69c97d2110d9936943f1d4372c9
[QiaoChenx] <[a32151@126.com]>
2022-12-02 10:29:17 +0800

    todo-list

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false;
 
-import store from './store';
 
 new Vue({
   render: h => h(App),
   beforeCreate() {
     Vue.prototype.$bus = this;
   },
-  store
 }).$mount('#app')
